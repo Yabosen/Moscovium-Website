272fcb96be867a0273a36019135825d1cc2b8bed
Aquamarine Hoshino <cozoral@outlook.com>
2025-11-24 21:48:34 +0100

    OpenBeta

--- src/app/config.ts
+++ src/app/config.ts
@@ -34,7 +34,7 @@ export const siteConfig: SiteConfig = {
       icon: MessageSquare,
     },
   ],
-  downloadLink: "https://pixeldrain.com/u/LwoeEj3r",
+  downloadLink: "https://github.com/Yabosen/MoscoviumDownload/releases/tag/V2.1-Open-Beta",
   donationLink: "https://www.tipeeestream.com/yabosen/donation",
   backgroundUrl: "",
 };
